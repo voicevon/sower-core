be6c7c3a9cad28d5ea5a02a851668d55cb939a21
unknown <voicevon@gmail.com>
2020-10-13 08:23:10 +0800

    minus

--- Python/sower_arm.py
+++ Python/sower_arm.py
@@ -1,4 +1,8 @@
 import sys
+sys.path.append('C:\\gitlab\\bot\\python\\reprap')  # on windows
+# sys.path.append('/home/xm/gitrepo/bot/python/reprap') # on linux
+
+from reprap_arm import ReprapArm
 
 class SowerArm(ReprapArm):
     '''
