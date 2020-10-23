d8eac06a8927832117c078523abd044a9bb8d00e
xuming <voicevon@gmail.com>
2020-10-23 07:40:23 -0500

    cell from map

--- Python/plate.py
+++ Python/plate.py
@@ -10,37 +10,47 @@ class PLATE_CELL_STATE(Enum):
 
 class PlateCell():
     def __init__(self):
-        self.state = PLATE_CELL_STATE.Emppty_Unplanned()
+        self.state = PLATE_CELL_STATE.Emppty_Unplanned
 
+    def from_cell_map(self, cell_map):
+        # at the moment , cell_map is bool
+        if cell_map:
+            self.state = PLATE_CELL_STATE.Emppty_Unplanned
+        else:
+            self.state = PLATE_CELL_STATE.Prefilled
+
+    def to_string(self):
+        table = {PLATE_CELL_STATE.Emppty_Unplanned:'. ',
+                 PLATE_CELL_STATE.Empty_Planned: '= ',
+                 PLATE_CELL_STATE.Prefilled: 'P ',
+                 PLATE_CELL_STATE.Refilled: 'R '
+                 }
+        return table[self.state]
 
 class PlateRow():
     '''
-    Composed by an array of cells
-    cell == True: means empty. 
+    Composed by an list of cells
     '''
-    def __init__(self):
-
-
+    def __init__(self, id):
+        self.id = id
         # self.cells=list(bool)
         self.cells=[]
         self.config_cols_lenth = 8
-        for i in range(0, self.config_cols_lenth):
-            self.cells.append(True)
+        # for i in range(0, self.config_cols_lenth):
+        #     self.cells.append(True)
 
 
     def from_row_map(self, row_map):
-        for i in range(0,8):
-            self.cells[i] = row_map[i]
+        for i in range(0,len(row_map)):
+            cell = PlateCell()
+            cell.from_cell_map(row_map[i])
+            self.cells.append(cell)    
     
-    
-    def print_out(self):
-        print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
-        out = ''
-        for c in self.cells:
-            if c:
-                out += 'O'
-            else:
-                out += '.'
+    def print_out(self, string_head, string_tail):
+        out = string_head
+        for cell in self.cells:
+            out += cell.to_string()
+        out += string_tail
         print(out)
 
 
@@ -72,6 +82,7 @@ class Plate():
         self.__PIN_ENCODER_B = 32
         self.next_enter_row_id = 0
         self.encoder_distance = 0
+        self.__encoder_distance_per_row = 200
 
     def setup(self):
         GPIO.setmode(GPIO.BOARD)
@@ -88,7 +99,7 @@ class Plate():
 
     def on_encoder_rising(self):
         self.encoder_distance += 1
-        if self.encoder_distance / 200 == 0:
+        if self.encoder_distance / self.__encoder_distance_per_row == 0:
             # current row must be fininshed. new row is coming
             self.next_enter_row_id += 1
 
@@ -99,24 +110,46 @@ class Plate():
         return self.rows[row_id]
 
     def from_map(self, cells_map): 
+        # print(cells_map)
         self.rows=[]
-        for i in range(0,8):
-            row = PlateRow()
+        for i in range(0,len(cells_map)):
+            row = PlateRow(i)
             row.from_row_map(cells_map[i])
             self.rows.append(row)
         self.state = PLATE_STATE.Mapped
 
     def print_out_map(self):
         for row in self.rows:
-            print('##################################')
-            row.print_out()
+            row_id_string = str(row.id)
+            if row.id <10:
+                row_id_string = '0' + row_id_string 
+            row.print_out(row_id_string + '--  ','')
 
     def main_loop(self):
-        
-        print(self.encoder_distance)
+        pass
+        # print(self.next_enter_row_id, self.encoder_distance)
 
 if __name__ == "__main__":
     plate = Plate()
     plate.setup()
+    map = [[True,True,True,True,False,True,False,True],
+            [True,True,True,True,True,True,True,True],
+            [True,True,True,True,True,True,True,True], 
+            [True,True,True,True,True,True,True,True],
+            [True,True,True,True,True,True,True,True],
+            [True,True,True,True,True,True,True,True],
+            [True,True,True,True,True,True,True,True],
+            [True,True,True,True,True,True,True,True],
+
+            [True,True,True,True,True,True,True,True],
+            [True,True,True,True,True,True,True,True],
+            [True,True,True,True,True,True,True,True],
+            [True,True,True,True,True,True,True,True],
+            [True,True,True,True,True,True,True,True],
+            [True,True,True,True,True,True,True,True],
+            [True,True,True,True,True,True,True,True],
+            [True,True,True,True,True,True,True,True]]
+    plate.from_map(map)
+    plate.print_out_map()
     while True:
         plate.main_loop()
