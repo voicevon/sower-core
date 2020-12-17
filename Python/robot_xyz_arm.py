6f03b09e14d39fb725bcd7348002fd7e59fd69b6
xuming <voicevon@gmail.com>
2020-12-17 10:52:46 +0800

    calibrated

--- Python/robot_xyz_arm.py
+++ Python/robot_xyz_arm.py
@@ -32,13 +32,23 @@ class XyzArm(ReprapArm, metaclass=Singleton):
 
     def __get_xy_from_col_row(self, col, row):
         '''
-        if row == -1, will return warehouse of X
+        Y
+        ^
+        |      col=0
+        |      col=1
+        |      ...
+        |      ...
+        |      col=5
+        |      col=6
+        |      col=7
+        |----- row=0,  row=1 ... row=-1  ------------------> X
+        if row == -1, will return x position of warehouse
 
         From manual calibration:  col,row :(0,0)  maps to x,y (30,20)
         '''
-        x = 30 + col * 32
-        y = 5 + (7-row) * 32
-        if col == -1:
+        x = 110 + row * 32
+        y = 15 + (7-col) * 32
+        if row == -1:
             x = 180
         return x, y
 
@@ -53,15 +63,15 @@ class XyzArm(ReprapArm, metaclass=Singleton):
         self.wait_for_movement_finsished()
         self.bridge_send_gcode_mcode('M106 S0')
 
-    def __move_to_warehouse(self, row):
-        x, y = self.__get_xy_from_col_row(-1, row)
+    
+    def pickup_from_warehouse(self, col):
+        WAREHOUSE_MOVEMENT_TIME = 1.5   # when warehouse moves from bottom to top(or reverse), will cost some time. 
+
+        # move_to_warehouse(col)
+        x, y = self.__get_xy_from_col_row(col, row=-1)
         self.move_to_xyz(x, y,speed_mm_per_min=18000)
         self.__current_y = y
 
-    def pickup_from_warehouse(self, row):
-        WAREHOUSE_MOVEMENT_TIME = 1.5   # when warehouse moves from bottom to top(or reverse), will cost some time. 
-
-        self.__move_to_warehouse(row)
         self.lift_warehouse()
         time.sleep(WAREHOUSE_MOVEMENT_TIME)
         self.drop_warehouse()
@@ -129,16 +139,17 @@ if __name__ == "__main__":
     my_arm.setup_and_home('/dev/ttyUSB0')
     my_arm.home_y_x()
     
-    if False:
-        my_arm.calibrate_col_row(2,7)
+    if True:
+        my_arm.calibrate_col_row(7,0)
 
     if False:
-        # moves a big squre
+        # moves a big squre/
+        MAX_SPEED = 18000
         while True:
-            my_arm.move_to_xyz(0, 0, 0, 18000)
-            my_arm.move_to_xyz(0, 280, 0, 18000)
-            my_arm.move_to_xyz(180, 280, 0, 18000)
-            my_arm.move_to_xyz(180, 0, 0, 18000)
+            my_arm.move_to_xyz(90, 10, 0, MAX_SPEED)
+            my_arm.move_to_xyz(90, 280, 0, MAX_SPEED)
+            my_arm.move_to_xyz(180, 280, 0, MAX_SPEED)
+            my_arm.move_to_xyz(180, 10, 0, MAX_SPEED)
 
     if False:
         # test warehouse
@@ -150,10 +161,10 @@ if __name__ == "__main__":
 
     if True:
         while True:
-            for row_id in range(0,3):
-                for col_id in range(0,8):
+            for col_id in range(7,-1,-1):
+                for row_id in range(0,2):
                     my_arm.pickup_from_warehouse(col_id)
-                    my_arm.place_to_cell(row_id, col_id)
+                    my_arm.place_to_cell(col_id, row=row_id)
 
 
 
